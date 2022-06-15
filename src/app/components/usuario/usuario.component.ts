2f82b5def3bc8dbf6c42146c1f2a0210cdf1d9e3
Byron Delgado <byrondanipm@gmail.com>
2022-06-15 17:14:54 -0500

    mostrar usuarios en el html

--- src/app/components/usuario/usuario.component.ts
+++ src/app/components/usuario/usuario.component.ts
@@ -9,15 +9,20 @@ import { Usuario } from 'src/app/models/usuario.model';
 export class UsuarioComponent implements OnInit {
   URL_API : string = "https://localhost:7111/api/usuario";
   usuarios : Usuario[] = []
+  saludo = "Hola!!";
   constructor() { }
 
   ngOnInit(): void {
     this.obtenerUsuarios().then(res=>{
       this.usuarios = res;
+      console.log(this.usuarios);
     });
   }
+
+  //Get to Api
   async obtenerUsuarios() : Promise <any>{
     let response = await fetch(`${this.URL_API}`);
-    return response.json();
+    let json = await response.json();
+    return json;
   }
 }

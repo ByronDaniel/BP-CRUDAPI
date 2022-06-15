8d3f0e95ce615f0067a2088c1ded5a89604d0e92
Byron Delgado <byrondanipm@gmail.com>
2022-06-15 16:43:39 -0500

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { UsuarioComponent } from './components/usuario/usuario.component';
+import { CuentaComponent } from './components/cuenta/cuenta.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsuarioComponent,
+    CuentaComponent
   ],
   imports: [
     BrowserModule
